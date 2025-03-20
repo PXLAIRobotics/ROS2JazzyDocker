a92cd1a8cee26ba0e9c5fef5fda395112a11510c
TimDupontPXL <tim.dupont@pxl.be>
2025-03-20 16:19:09 +0100

    Fix for building.

--- pxl_ros2_jazzy_image/01_build_image.sh
+++ pxl_ros2_jazzy_image/01_build_image.sh
@@ -3,10 +3,11 @@ if [[ "$(uname)" == "Darwin" ]]; then
     echo "Running this script on macOS..."
     docker build \
            --platform linux/amd64 \
+           --build-arg UID=$UID \
            -t pxl_ros2_jazzy:latest .
 else
     echo "Running this script on Windows/Linux..."
     docker build \
-           --build-arg UID=$UID
+           --build-arg UID=$UID \
            -t pxl_ros2_jazzy:latest .
 fi
